a311a09a53f7e828fc2e2746aa7746973b0ca49e
mirkomikac <mmikac27@gmail.com>
2017-06-20 11:49:15 +0200

    HTTPS support.

--- CentralBank/src/main/java/com/CentralBankApplication.java
+++ CentralBank/src/main/java/com/CentralBankApplication.java
@@ -1,13 +1,24 @@
 package com;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.config.SSLCertificateValidation;
 
 @SpringBootApplication
 public class CentralBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentralBankApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner lookup() {
+		return args -> {
+			SSLCertificateValidation.disable();
+		};
 	}
 	
 }
